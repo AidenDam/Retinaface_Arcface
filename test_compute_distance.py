589c5f892e59dfc6dd4a6f2c9591369b11befb5b
AidenDam <AidenDaniel827@gmail.com>
2022-09-16 08:54:24 +0700

    face recognition

---
+++ test_compute_distance.py
@@ -0,0 +1,20 @@
+import time
+from retinaface import Face_Recognition
+
+tic = time.time()
+
+dst = Face_Recognition.verify('./test/data/test3.jpg', './test/data/test2.jpg')
+
+tac = time.time()
+
+print(f'Pass test 1: [time: {tac-tic} res: {dst}]')
+
+#######################################
+
+tic = time.time()
+
+dst = Face_Recognition.verify([['./test/data/test3.jpg', './test/data/test2.jpg'], ['./test/data/test1.jpg', './test/data/test2.jpg']])
+
+tac = time.time()
+
+print(f'Pass test 2: [time: {tac-tic} res: {dst}]')
